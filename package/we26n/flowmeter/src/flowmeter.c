159c9f4bffb4ba5de1b67ba9ce10a25f499867e4
wang mingdong <wmingdong@teleepoch.com>
2015-06-19 19:15:17 +0800

    add we26n soft package dir

---
+++ package/we26n/flowmeter/src/flowmeter.c
@@ -0,0 +1,23 @@
+
+
+#include <sys/types.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+
+#include <time.h>
+#include <limits.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+
+int  main( void )
+{
+	
+	printf("hello \r\n");
+	return 0;
+}
+
+
