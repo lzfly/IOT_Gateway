65090f57e3cc122f878a32917e3b878b4011ea51
wang mingdong <wmingdong@teleepoch.com>
2015-06-26 14:51:33 +0800

    add webbridge lua files

---
+++ package/we26n/webbridge/files/commanddispatcher.lua
@@ -0,0 +1,105 @@
+require "ubus"
+
+http = require "socket.http";
+url = "http://192.168.1.7:8081";
+
+conn = ubus.connect();
+if not conn then
+	error("Failed to connect to ubusd");
+end
+
+function getCommand()
+	resp, code = http.request(url);
+	if code == 200 then
+		return resp;
+	end
+
+	return nil;
+end
+
+function sendToZigbeeJianyou(gatewayid_value, deviceid_value, attr_value, data_value)
+--[[
+	local result = conn:call("we26n_zigbee_febee", "ctrlcmd", { gatewayid = gatewayid_value, deviceid = deviceid_value, devicetype = "0012", attr = attr_value, data = data_value });
+	if result ~= nil then
+		for k, v in pairs(result) do
+			print("key=" .. k .. " value=" .. tostring(v));
+		end
+	else
+		print("sendToZigbeeJianyou return nil.");
+	end
+]]
+end
+
+function sendToZigbeeFbee(gatewayid_value, deviceid_value, attr_value, data_value)
+	local result = conn:call("we26n_zigbee_febee", "ctrlcmd", { gatewayid = gatewayid_value, deviceid = deviceid_value, devicetype = "0012", attr = attr_value, data = data_value });
+	if result ~= nil then
+		for k, v in pairs(result) do
+			print("key=" .. k .. " value=" .. tostring(v));
+		end
+	else
+		print("sendToZigbeeFbee return nil.");
+	end
+end
+
+function sendTo433Jianyou(gatewayid_value, deviceid_value, attr_value, data_value)
+--[[
+	local result = conn:call("we26n_zigbee_febee", "ctrlcmd", { gatewayid = gatewayid_value, deviceid = deviceid_value, devicetype = "0012", attr = attr_value, data = data_value });
+	if result ~= nil then
+		for k, v in pairs(result) do
+			print("key=" .. k .. " value=" .. tostring(v));
+		end
+	else
+		print("sendTo433Jianyou return nil.");
+	end
+]]
+end
+
+function dispatchCommand(data)
+	
+	local cjson = require "cjson";
+	local jobj = cjson.decode(data);
+
+	for idx, value in ipairs(jobj) do
+		local devId = value["deviceid"]
+		if devId ~= nil then
+			local gatewayId = value["gatewayid"];
+			local attr = value["attr"];
+			local data = value["data"];
+
+			local devIdLower = string.lower(devId);
+			if string.match(devIdLower, "zigbee_jianyou") == "zigbee_jianyou" then
+				sendToZigbeeJianyou(gatewayId, devId, attr, data);
+			elseif string.match(devIdLower, "zigbee_fbee") == "zigbee_fbee" then
+				sendToZigbeeFbee(atewayId, devId, attr, data);
+			elseif string.match(devIdLower, "433_jianyou") == "433_jianyou" then
+				sendTo433Jianyou(gatewayId, devId, attr, data);
+			end
+		end
+	end
+
+end
+
+local processing = false;
+local t = os.time();
+
+while true do
+	local time = os.time();
+	if time - t >= 1 and not processing then
+		processing = true;
+
+		local result = getCommand();
+
+		--result = '[{ "gatewayid":"we26n_xxxxxx", "deviceid":"zigbee_fbee_xxxxx", "attr":"001", "data":"989797897897897" },{ "gatewayid":"we26n_xxxxxx", "deviceid":"zigbee_jianyou_xxxxx", "attr":"001", "data":"989797897897897" }]';
+
+		if result ~= nil then
+			if not pcall(dispatchCommand, result) then
+				print("dispatch command error....");
+			end
+		else
+			print("get command error....");
+		end
+
+		processing = false;
+		t = time;
+	end
+end
---
+++ package/we26n/webbridge/files/datareport.lua
@@ -0,0 +1,71 @@
+require "ubus"
+require "uloop"
+
+uloop.init();
+
+local conn = ubus.connect();
+if not conn then
+	error("Failed to connect tot ubus.");
+end
+
+http = require "socket.http";
+
+local customMethod = {
+	jianyou = {
+		report = {
+			function(req, msg)
+				local queryParam = "gatewayid=";
+				
+				local gatewayId = msg["gatewayid"];
+				if gatewayId ~= nil then
+					queryParam = queryParam .. gatewayId;
+				end
+
+				queryParam = queryParam .. "&deviceid=";
+				local deviceId = msg["deviceid"];
+				if deviceId ~= nil then
+					queryParam = queryParam .. deviceId;
+				end
+				
+				queryParam = queryParam .. "&devicetype=";
+				local deviceType = msg["devicetype"];
+				if deviceType ~= nil then
+					queryParam = queryParam .. deviceType;
+				end
+
+				queryParam = queryParam .. "&attr=";
+				local attr = msg["attr"];
+				if attr ~= nil then
+					queryParam = queryParam .. attr;
+				end
+
+				queryParam = queryParam .. "&data=";
+				local data = msg["data"];
+				if data ~= nil then
+					queryParam = queryParam .. data;
+				end
+
+				if queryParam ~= nil then
+					local targetUrl = "http://192.168.1.7:8081/helloapp/info?" .. queryParam;
+
+					print(targetUrl);
+
+					for i=1,3 do
+						resp, code = http.request(targetUrl);
+						if code == 200 then
+							conn:reply(req, {code="S00000", message="" .. resp});
+							return;
+						end
+					end
+
+					conn:reply(req, {code="E00002", message="" .. code});
+				else
+					conn:reply(req, {code="E00001", message="Parameters 'gatewayid、deviceid、devicetype、attr、data' can not be empty."});
+				end
+			end, {gatewayid = ubus.STRING, deviceid = ubus.STRING, devicetype = ubus.STRING, attr = ubus.STRING, data = ubus.STRING}
+		}
+	}
+}
+conn:add(customMethod);
+
+uloop.run();
