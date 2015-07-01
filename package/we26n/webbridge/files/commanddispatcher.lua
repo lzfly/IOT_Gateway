e7cb1284541a8d210093800f247b63c5025240e0
wang mingdong <wmingdong@teleepoch.com>
2015-07-01 18:01:28 +0800

    fix command get ip addr

--- package/we26n/webbridge/files/commanddispatcher.lua
+++ package/we26n/webbridge/files/commanddispatcher.lua
@@ -8,7 +8,7 @@ macAddr = string.gsub(macAddr, ":", "");
 macAddr = "we26n_" .. macAddr;
 
 http = require "socket.http";
-url = "http://192.168.1.26:8080/helloapp/ctrl?gatewayid=" .. macAddr;
+url = "http://101.200.1.101:8001/helloapp/ctrl?gatewayid=" .. macAddr;
 
 conn = ubus.connect();
 if not conn then
