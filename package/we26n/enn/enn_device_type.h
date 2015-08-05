c6fc2ba6253167139a114656b34211930603028e
dwz <weizheng.duan@jianluyou.com>
2015-08-05 14:08:56 +0800

    Add enn device type

--- package/we26n/enn/enn_device_type.h
+++ package/we26n/enn/enn_device_type.h
@@ -1,36 +1,28 @@
 #ifndef ENN_DEVICE_TYPE_H
 #define ENN_DEVICE_TYPE_H
-
+//窗帘 
+#define ENN_DEVICE_TYPE_WINDOWS "0002"
+//PM2.5
+#define ENN_DEVICE_TYPE_PM25 "0003"
+//甲醛报警
+#define ENN_DEVICE_TYPE_FORMAL "0004"
+//燃气报警
+#define ENN_DEVICE_TYPE_GAS "0005"
+//烟雾
+#define ENN_DEVICE_TYPE_SMOKE "0006"
+//电表
+#define ENN_DEVICE_TYPE_POWERMETER "0007"
 
 //水表
 #define ENN_DEVICE_TYPE_WATERMETER "0008"
-
-//电表
-#define ENN_DEVICE_TYPE_POWERMETER "0007"
+//温湿度 
+#define ENN_DEVICE_TYPE_TEMP_HUM "0009"
+//可调亮度色温灯 
+#define ENN_DEVICE_TYPE_COLOR_TEMP_LAMP "0015"
 
 //三控开关
 #define ENN_DEVICE_TYPE_ON_OFF_THREE "0016"
 
-//窗帘 
-#define ENN_DEVICE_TYPE_WINDOWS "0002"
-
-//可调亮度色温灯 
-#define ENN_DEVICE_TYPE_COLOR_TEMP_LAMP "0015"
-
-//温湿度 
-#define ENN_DEVICE_TYPE_TEMP_HUM "0009"
-
-//燃气报警
-#define ENN_DEVICE_TYPE_GAS "0005"
-
-//甲醛报警
-#define ENN_DEVICE_TYPE_FORMAL "0004"
-
-//PM2.5
-#define ENN_DEVICE_TYPE_PM25 "0003"
-
-//烟雾
-#define ENN_DEVICE_TYPE_SMOKE "0006"
 //开关插座
 #define ENN_DEVICE_TYPE_POWER_OUTLET "0017"
 
@@ -38,5 +30,10 @@
 #define ENN_DEVICE_TYPE_MAGNETIC_DOOR "0018"
 //人体红外
 #define ENN_DEVICE_TYPE_BODY_INFRARED "0019"
+//燃气表
+#define ENN_DEVICE_TYPE_GAS_METER "0020"
+//热表
+#define ENN_DEVICE_TYPE_HOTLIST "0021"
+
 
 #endif
