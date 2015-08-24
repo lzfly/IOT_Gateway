ef8d59d61a733f0ee9f642a87d4393e844c16a36
luoie <luoiefly@163.com>
2015-08-24 23:12:18 +0800

    add crc check for wifibridge_1

--- package/we26n/wifibridge_1/src/server.c
+++ package/we26n/wifibridge_1/src/server.c
@@ -255,6 +255,82 @@ int32_t  modbus_conv_long( uint8_t * puc )
     /**/
     return temp;
 }
+uint8_t auchCRCHi[] = {
+0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41, 0x01, 0xC0,
+0x80, 0x41, 0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41,
+0x00, 0xC1, 0x81, 0x40, 0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0,
+0x80, 0x41, 0x01, 0xC0, 0x80, 0x41, 0x00, 0xC1, 0x81, 0x40,
+0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41, 0x00, 0xC1,
+0x81, 0x40, 0x01, 0xC0, 0x80, 0x41, 0x01, 0xC0, 0x80, 0x41,
+0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41, 0x00, 0xC1,
+0x81, 0x40, 0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41,
+0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41, 0x01, 0xC0,
+0x80, 0x41, 0x00, 0xC1, 0x81, 0x40, 0x00, 0xC1, 0x81, 0x40,
+0x01, 0xC0, 0x80, 0x41, 0x01, 0xC0, 0x80, 0x41, 0x00, 0xC1,
+0x81, 0x40, 0x01, 0xC0, 0x80, 0x41, 0x00, 0xC1, 0x81, 0x40,
+0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41, 0x01, 0xC0,
+0x80, 0x41, 0x00, 0xC1, 0x81, 0x40, 0x00, 0xC1, 0x81, 0x40,
+0x01, 0xC0, 0x80, 0x41, 0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0,
+0x80, 0x41, 0x01, 0xC0, 0x80, 0x41, 0x00, 0xC1, 0x81, 0x40,
+0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41, 0x01, 0xC0,
+0x80, 0x41, 0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41,
+0x00, 0xC1, 0x81, 0x40, 0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0,
+0x80, 0x41, 0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41,
+0x01, 0xC0, 0x80, 0x41, 0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0,
+0x80, 0x41, 0x00, 0xC1, 0x81, 0x40, 0x00, 0xC1, 0x81, 0x40,
+0x01, 0xC0, 0x80, 0x41, 0x01, 0xC0, 0x80, 0x41, 0x00, 0xC1,
+0x81, 0x40, 0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41,
+0x00, 0xC1, 0x81, 0x40, 0x01, 0xC0, 0x80, 0x41, 0x01, 0xC0,
+0x80, 0x41, 0x00, 0xC1, 0x81, 0x40
+};
+
+uint8_t auchCRCLo[] = {
+0x00, 0xC0, 0xC1, 0x01, 0xC3, 0x03, 0x02, 0xC2, 0xC6, 0x06,
+0x07, 0xC7, 0x05, 0xC5, 0xC4, 0x04, 0xCC, 0x0C, 0x0D, 0xCD,
+0x0F, 0xCF, 0xCE, 0x0E, 0x0A, 0xCA, 0xCB, 0x0B, 0xC9, 0x09,
+0x08, 0xC8, 0xD8, 0x18, 0x19, 0xD9, 0x1B, 0xDB, 0xDA, 0x1A,
+0x1E, 0xDE, 0xDF, 0x1F, 0xDD, 0x1D, 0x1C, 0xDC, 0x14, 0xD4,
+0xD5, 0x15, 0xD7, 0x17, 0x16, 0xD6, 0xD2, 0x12, 0x13, 0xD3,
+0x11, 0xD1, 0xD0, 0x10, 0xF0, 0x30, 0x31, 0xF1, 0x33, 0xF3,
+0xF2, 0x32, 0x36, 0xF6, 0xF7, 0x37, 0xF5, 0x35, 0x34, 0xF4,
+0x3C, 0xFC, 0xFD, 0x3D, 0xFF, 0x3F, 0x3E, 0xFE, 0xFA, 0x3A,
+0x3B, 0xFB, 0x39, 0xF9, 0xF8, 0x38, 0x28, 0xE8, 0xE9, 0x29,
+0xEB, 0x2B, 0x2A, 0xEA, 0xEE, 0x2E, 0x2F, 0xEF, 0x2D, 0xED,
+0xEC, 0x2C, 0xE4, 0x24, 0x25, 0xE5, 0x27, 0xE7, 0xE6, 0x26,
+0x22, 0xE2, 0xE3, 0x23, 0xE1, 0x21, 0x20, 0xE0, 0xA0, 0x60,
+0x61, 0xA1, 0x63, 0xA3, 0xA2, 0x62, 0x66, 0xA6, 0xA7, 0x67,
+0xA5, 0x65, 0x64, 0xA4, 0x6C, 0xAC, 0xAD, 0x6D, 0xAF, 0x6F,
+0x6E, 0xAE, 0xAA, 0x6A, 0x6B, 0xAB, 0x69, 0xA9, 0xA8, 0x68,
+0x78, 0xB8, 0xB9, 0x79, 0xBB, 0x7B, 0x7A, 0xBA, 0xBE, 0x7E,
+0x7F, 0xBF, 0x7D, 0xBD, 0xBC, 0x7C, 0xB4, 0x74, 0x75, 0xB5,
+0x77, 0xB7, 0xB6, 0x76, 0x72, 0xB2, 0xB3, 0x73, 0xB1, 0x71,
+0x70, 0xB0, 0x50, 0x90, 0x91, 0x51, 0x93, 0x53, 0x52, 0x92,
+0x96, 0x56, 0x57, 0x97, 0x55, 0x95, 0x94, 0x54, 0x9C, 0x5C,
+0x5D, 0x9D, 0x5F, 0x9F, 0x9E, 0x5E, 0x5A, 0x9A, 0x9B, 0x5B,
+0x99, 0x59, 0x58, 0x98, 0x88, 0x48, 0x49, 0x89, 0x4B, 0x8B,
+0x8A, 0x4A, 0x4E, 0x8E, 0x8F, 0x4F, 0x8D, 0x4D, 0x4C, 0x8C,
+0x44, 0x84, 0x85, 0x45, 0x87, 0x47, 0x46, 0x86, 0x82, 0x42,
+0x43, 0x83, 0x41, 0x81, 0x80, 0x40
+};
+uint16_t  modbus_crc16( uint8_t * puchMsg, int tlen )
+{
+    uint8_t  uchCRCHi=0xFF;  /* žßCRC×ÖœÚ³õÊŒ»¯ */ 
+    uint8_t  uchCRCLo=0xFF;  /* µÍCRC ×ÖœÚ³õÊŒ»¯ */ 
+    uint32_t  uIndex;        /* CRCÑ­»·ÖÐµÄË÷Òý */ 
+    
+    while ( tlen--)
+    {
+        uIndex = uchCRCHi^*puchMsg++;
+        uchCRCHi = uchCRCLo ^ auchCRCHi[uIndex];
+        uchCRCLo = auchCRCLo[uIndex];
+    }
+
+    /**/
+    uIndex = uchCRCHi;
+    uIndex = uIndex << 8;
+    uIndex = uIndex | uchCRCLo;
+    return (uint16_t)uIndex;
+}
 
 #define MAXINTERFACES   16
 char g_localMAC[16];
@@ -527,7 +603,7 @@ void* enn_meter_thread( void *arg )
 	uint8_t * puc_power,* puc_water,*puc_heat;
 	double d,aaa_h;
 	int connectfd;
-	
+	uint16_t crcvalue, crcvalue1;
 	
     	char buf_power[N],buf_water[N],buf_heat[N];
      	char buff_power[N] = {0x01,0X03,0X00,0X14,0X00,0X02,0X84,0X0F};
@@ -548,22 +624,30 @@ void* enn_meter_thread( void *arg )
 	while(1)
 	{      
 		printf("in while \n");
+                do{
+                    i = recv(connectfd,buf_power,18,0);
+                }while(i > 0);
 		//电表
 		sleep(2);
 		printf("sleep ok\n");
 		j=send(connectfd,buff_power,8,0);
 		if(j<0)
-	        continue;
+	            break;
 	        syslog(LOG_CRIT,"[power_meter]power meter  send ok");
 	        printf("power meter send ok\n");
 		int len_p=0,count_p=0;
+                char buff2[32];
+
 	    	while(count_p < 5 && len_p < 9)
 	    	{
-			i=recv(connectfd,buf_power,9,0);
+			i=recv(connectfd,buff2,9,0);
 			count_p++;
 			if(i<=0)
 			    continue;
+
+                        memcpy(buf_power+len_p, buff2, i);
 			len_p = len_p + i;
+                        sleep(1);
 			
 			
 		}
@@ -571,99 +655,142 @@ void* enn_meter_thread( void *arg )
 		{
 			syslog(LOG_CRIT,"[power_meter]power meter read not 9");
 			printf("power meter read not 9\n");
-			continue;
+			break;
 		}
 		  printf("power meter recv ok\n");
 		  syslog(LOG_CRIT,"[power_meter]power meter  recv ok");
 		printf("count_p=%d\n",count_p);
 		dump_hex(buf_power,len_p);
-		puc_power = (uint8_t *)buf_power;
-		aaa = modbus_conv_long(&(puc_power[3]));
-		//ddd = aaa*250*60;
-		//ddd = ddd/18000000;
-                ddd = aaa/(18000000/(250*60));
 
+                
 
-		printf("The power meter reading is :%lf\n",ddd);
-		sendMsgToWeb(powerid,POWER,ddd);
-		sleep(timep);
+                crcvalue = modbus_crc16( buf_power, len_p - 2 );
+                printf("crcvalue = %x\n", crcvalue);
+                crcvalue1 = ((buf_power[len_p - 2]&0xff) << 8) | (buf_power[len_p - 1] & 0xff);
+                printf("crcvalue1 = %x\n", crcvalue1);
+                if(crcvalue == crcvalue1)
+                {
+		    puc_power = (uint8_t *)buf_power;
+		    aaa = modbus_conv_long(&(puc_power[3]));
+		    //ddd = aaa*250*60;
+		    //ddd = ddd/18000000;
+                    ddd = aaa/(18000000/(250*60));
+
+		    printf("The power meter reading is :%lf\n",ddd);
+                    syslog(LOG_CRIT,"[water_meter]power meter send value=%f", ddd);
+		    sendMsgToWeb(powerid,POWER,ddd);
+                }
+                    
+                
+
+		sleep(timep*20);
 		printf("power sleep time:%d\n",timep);
 		printf("sleep ok\n");
 		//水表
 		
 		p=send(connectfd,buff_water,8,0);
 		if(p<0)
-	        continue;
+	            break;
 	        syslog(LOG_CRIT,"[water_meter]water meter  send ok");
 	        printf("water meter send ok\n");
 		int len_w=0,count_w=0;
 	    	while(count_w < 5 && len_w < 13)
 	    	{
-			q=recv(connectfd,buf_water,13,0);
+			q=recv(connectfd,buff2,13,0);
 			count_w++;
 			if(q<=0)
 			    continue;
+
+                        memcpy(buf_water+len_w, buff2, q);
 			len_w = len_w + q;
+                        sleep(1);
 			
 		}
 		if(len_w != 13)
 		{
 			syslog(LOG_CRIT,"[water_meter]water meter read not 13");
 			printf("water meter read not 13\n");
-			continue;
+			break;
 		}
 		printf("water meter recv ok\n");
 		printf("count_w=%d\n",count_w);
 		dump_hex(buf_water,len_w);
 		syslog(LOG_CRIT,"[water_meter]water meter  recv ok");
-		puc_water = (uint8_t *)buf_water;
-		aaa_w = modbus_conv_longm(&(puc_water[3]));
-		bbb = modbus_conv_real4(&(puc_water[7]));
-		ccc = bbb;
-		ccc = ccc + aaa_w; 
-		printf("The water meter reading is :%f\n",ccc);
-		sendMsgToWeb(waterid,WATER,ccc);
-		sleep(timew);
+
+
+                crcvalue = modbus_crc16( buf_water, len_w - 2 );
+                printf("crcvalue = %x\n", crcvalue);
+                crcvalue1 = ((buf_water[len_w - 2]&0xff) << 8) | (buf_water[len_w - 1] & 0xff);
+                printf("crcvalue1 = %x\n", crcvalue1);
+                if(crcvalue == crcvalue1)
+                {
+		    puc_water = (uint8_t *)buf_water;
+		    aaa_w = modbus_conv_longm(&(puc_water[3]));
+		    bbb = modbus_conv_real4(&(puc_water[7]));
+		    ccc = bbb;
+		    ccc = ccc + aaa_w; 
+		    printf("The water meter reading is :%f\n",ccc);
+                    syslog(LOG_CRIT,"[water_meter]water meter send value=%f", ccc);
+		    sendMsgToWeb(waterid,WATER,ccc);
+                }
+
+
+		sleep(timew*20);
 		printf("water sleep time:%d\n",timew);
 		
 		//热表
 		m=send(connectfd,buff_heat,8,0);
 		if(m<0)
-	        continue;
+	            break;
 	        syslog(LOG_CRIT,"[heat_meter]heat meter  send ok");
 	        printf("heat meter send ok\n");
 		int len_h=0,count_h=0;
 	    	while(count_h < 5 && len_h < 13)
 	    	{
-			n=recv(connectfd,buf_heat,13,0);
+			n=recv(connectfd,buff2,13,0);
 			count_h++;
 			if(n<=0)
 			    continue;
+                        memcpy(buf_heat+len_h, buff2, n);
 			len_h = len_h + n;
 			//printf("\nlen_h = %d,count_h = %d\n")
+                        sleep(1);
 			
 		}
 		if(len_h != 13)
 		{
 			syslog(LOG_CRIT,"[heat_meter]heat meter read not 13");
 			printf("heat meter read not 13\n");
-			continue;
+			break;
 		}
 		printf("heat meter recv ok\n");
 		syslog(LOG_CRIT,"[heat_meter]heat meter  recv ok");
 		printf("\n*****heat meter*******\n");
 		dump_hex(buf_heat,len_h);
-		puc_heat = (uint8_t *)buf_heat;
-		aaa_h = modbus_conv_long(&(puc_heat[3]));
-		bbb_h = modbus_conv_real4(&(puc_heat[7]));
-		printf("\n************\n");
-		printf("aaa_h = %ld\n",aaa_h);
-		printf("\n************\n");
-		ccc_h = bbb_h;
-		ccc_h = ccc_h + aaa_h;
-		printf("The heat meter reading is :%f\n",ccc_h);
-		sendMsgToWeb(heatid,HEAT,ccc_h);
-		sleep(timeh);
+
+
+                crcvalue = modbus_crc16( buf_heat, len_h - 2 );
+                printf("crcvalue = %x\n", crcvalue);
+                crcvalue1 = ((buf_heat[len_h - 2]&0xff) << 8) | (buf_heat[len_h - 1] & 0xff);
+                printf("crcvalue1 = %x\n", crcvalue1);
+                if(crcvalue == crcvalue1)
+                {
+		    puc_heat = (uint8_t *)buf_heat;
+		    aaa_h = modbus_conv_long(&(puc_heat[3]));
+		    bbb_h = modbus_conv_real4(&(puc_heat[7]));
+		    printf("\n************\n");
+		    printf("aaa_h = %ld\n",aaa_h);
+		    printf("\n************\n");
+		    ccc_h = bbb_h;
+		    ccc_h = ccc_h + aaa_h;
+		    printf("The heat meter reading is :%f\n",ccc_h);
+                    syslog(LOG_CRIT,"[water_meter]heat meter send value=%f", ccc_h);
+		    sendMsgToWeb(heatid,HEAT,ccc_h);
+                }
+
+
+
+		sleep(timeh*20);
 		printf("heat sleep time:%d\n",timeh);
 		
 		sprintf(&devicesstr[0], "[");
