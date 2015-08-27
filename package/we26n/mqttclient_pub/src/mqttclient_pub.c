2755a531e380638d8e24b659b784487f23fc4dfe
luz <luz@luz-server.(none)>
2015-08-27 15:07:33 +0800

    mqttclient_pub  read config file

--- package/we26n/mqttclient_pub/src/mqttclient_pub.c
+++ package/we26n/mqttclient_pub/src/mqttclient_pub.c
@@ -33,9 +33,17 @@ Contributors:
 #include <libubox/uloop.h>
 #include <libubus.h>
 
+#include <uci.h>
+
 #define MAX_MESSAGE 129
 
-struct uloop_fd ufd;
+static struct uci_context * uci_ctx;
+static struct uci_package * uci_jianyoucfg;
+
+#define MAXTOPIC 128
+char mqtt_server[MAXTOPIC];
+char mqtt_port[MAXTOPIC];
+char mqtt_topic[MAXTOPIC];
 
 enum {
     ALERT_MODULE,
@@ -90,7 +98,8 @@ static int mqttclient_pub_alertnotice( struct ubus_context *ctx, struct ubus_obj
 	
 
 	char mqttpubcmd[180];
-	sprintf(mqttpubcmd, "mosquitto_pub -h 101.200.1.101 -p 8020 -t sensor  -m  \"%s\"", message);
+	//sprintf(mqttpubcmd, "mosquitto_pub -h 101.200.1.101 -p 8020 -t sensor  -m  \"%s\"", message);
+	sprintf(mqttpubcmd, "mosquitto_pub -h %s -p %s -t %s  -m  \"%s\"", mqtt_server, mqtt_port, mqtt_topic, message);
 	system(mqttpubcmd);
 	
 done:
@@ -166,8 +175,88 @@ int  ubus_server_init( void )
     
 }
 
+int get_mqttclient_config()
+{
+    int rtn = 0;
+    if (!uci_ctx)
+    {
+        uci_ctx = uci_alloc_context();
+    }
+    else
+    {
+        uci_jianyoucfg = uci_lookup_package(uci_ctx, "jyconfig");
+        if (uci_jianyoucfg)
+            uci_unload(uci_ctx, uci_jianyoucfg);
+    }
+
+    if (uci_load(uci_ctx, "jyconfig", &uci_jianyoucfg))
+    {
+        printf("uci load jianyou config fail\n");
+		return -1;
+    }
+	else
+	{
+	        char *value = NULL;
+            struct uci_element *e   = NULL;
+			int rtn;
+            printf("uci load jianyou config success\n");
+
+
+            /* scan jianyou config ! */
+            uci_foreach_element(&uci_jianyoucfg->sections, e)
+            {
+                struct uci_section *s = uci_to_section(e);
+
+                if(0 == strcmp(s->type, "mqttclient"))
+                {
+                    printf("%s(), type: %s\n", __FUNCTION__, s->type);
+
+                    value = uci_lookup_option_string(uci_ctx, s, "server");
+                    if(value){
+                            sprintf(mqtt_server, "%s", value);
+                            printf("%s(), mqtt_server: %s\n", __FUNCTION__, mqtt_server);
+                            rtn = 0;
+                        }else{
+                            printf("%s(), mqtt_server not found\n", __FUNCTION__);
+							rtn = -1;
+							goto exit;
+                     }
+					 
+					value = uci_lookup_option_string(uci_ctx, s, "port");
+                    if(value){
+                            sprintf(mqtt_port, "%s", value);
+                             printf("%s(), mqtt_port: %s\n", __FUNCTION__, mqtt_port);
+                        }else{
+                            printf("%s(), mqtt_port not found\n", __FUNCTION__);
+							rtn = -1;
+							goto exit;
+                     }
+					 
+					value = uci_lookup_option_string(uci_ctx, s, "userid");
+                    if(value){
+							sprintf(mqtt_topic, "%s", value);
+                            printf("%s(), mqtt_topic: %s\n", __FUNCTION__, mqtt_topic);
+
+                        }else{
+                            printf("%s(), mqtt_topic not found\n", __FUNCTION__);
+							rtn = -1;
+							goto exit;
+                     }
+					 
+                     break;
+                 }
+
+             }
+             
+    }
+exit:
+    uci_unload(uci_ctx, uci_jianyoucfg);
+    return rtn;
+}
+
 int main(int argc, char *argv[])
 {
+    get_mqttclient_config();
 	ubus_server_init();
 	return 0;
 }
