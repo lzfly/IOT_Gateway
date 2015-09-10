b5a5e737b444013914891df773e2b415c926ca1f
luz <luz@luz-server.(none)>
2015-09-10 11:05:31 +0800

    1. add curl package 2. modify ennconfig download

--- package/we26n/webbridge/files/ennconfig_download.lua
+++ package/we26n/webbridge/files/ennconfig_download.lua
@@ -6,8 +6,26 @@ function sleep(n)
 	os.execute("sleep " .. n)
 end
 
+function file_exists(filename)
+    local file = io.open(filename,"rb")
+    if file then
+        file:close()
+        return file
+    end
+    return nil
+end
+
 while true do
-    luci.sys.exec("rm -rf /etc/config/ennconfig")
+
+    luci.sys.exec("mv /etc/config/ennconfig /etc/config/ennconfig_back")
     luci.sys.exec("curl -o /etc/config/ennconfig 10.4.44.210:8001/ennconfig")
+    
+    file = file_exists("/etc/config/ennconfig")
+    if file then 
+        luci.sys.exec("rm -rf /etc/config/ennconfig_back")
+    else
+        luci.sys.exec("mv /etc/config/ennconfig_back /etc/config/ennconfig")
+    end
+
     sleep(86400)
 end
