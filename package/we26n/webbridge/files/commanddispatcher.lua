7cd5d21d4f037dba565a9117f1469bd8797657d4
luoie <luoiefly@163.com>
2015-12-01 22:09:56 +0800

    modify for new web server interface

--- package/we26n/webbridge/files/commanddispatcher.lua
+++ package/we26n/webbridge/files/commanddispatcher.lua
@@ -12,9 +12,12 @@ local macReader = io.popen("eth_mac r wifi");
 local macAddr = macReader:read("*all");
 macAddr = string.gsub(macAddr, ":", "");
 macAddr = "we26n_" .. macAddr;
+macAddr = string.gsub(macAddr,"%\r","")
+macAddr = string.gsub(macAddr,"%\n","")
+
 
 http = require "socket.http";
-url = "http://10.4.44.210:8001/enngateway/ctrl?gatewayid=" .. macAddr;
+url = "http://123.57.206.2:8081/v1/deviceattrconfigs?query=gateway_sn:eq:" .. macAddr .. ",is_control:eq:1";
 
 conn = ubus.connect();
 if not conn then
@@ -83,11 +86,11 @@ function dispatchCommand(data)
 
 	for idx, value in ipairs(jobj) do
 		if value ~= nil then
-			local devId = value["deviceid"]
+			local devId = value["device_sn"]
 			if devId ~= nil then
-				local gatewayId = value["gatewayid"];
-				local attr = value["attr"];
-				local data = value["data"];
+				local gatewayId = value["gateway_sn"];
+				local attr = value["attr_code"];
+				local data = value["attr_value"];
 
 				local devIdLower = string.lower(devId);
 				if string.match(devIdLower, "zigbee_jianyou") == "zigbee_jianyou" then
@@ -116,7 +119,7 @@ function getWebServerURL()
     print(ip)
     print(port)
     if iplen ~= 0 and portlen ~= 0 then
-        url = "http://" .. ip .. ":" .. port .. "/enngateway/ctrl?gatewayid=" .. macAddr;
+        url = "http://" .. ip .. ":" .. port .. "/v1/deviceattrconfigs?query=gateway_sn:eq:" .. macAddr .. ",is_control:eq:1";
     end
     print(url)
 
@@ -143,10 +146,10 @@ function  commanddispather()
 			print("get command error....");
 		end
 		
-		ttmr:set(60000);
+		ttmr:set(1000);
 end		
 
-ttmr = uloop.timer( commanddispather, 3000 );
+ttmr = uloop.timer( commanddispather, 1000 );
 	
 local customMethod = {
 	we26n_commanddispather = {
--- package/we26n/webbridge/files/datareport.lua
+++ package/we26n/webbridge/files/datareport.lua
@@ -13,7 +13,7 @@ end
 http = require "socket.http";
 
 
-url = "http://10.4.44.210:8001/enngateway/info?"
+url = "http://123.57.206.2:8081/v1/devicereports"
 gatewayId = "we26n_78A351097F2E"
 
 function getWebServerURL()
@@ -29,7 +29,7 @@ function getWebServerURL()
     print(ip)
     print(port)
     if iplen ~= 0 and portlen ~= 0 then
-        url = "http://" .. ip .. ":" .. port .. "/enngateway/info?";
+        url = "http://" .. ip .. ":" .. port .. "/v1/devicereports";
     end
     print(url)
 
@@ -67,42 +67,54 @@ local customMethod = {
 				end
 
 				--local deviceid_str = x:get("devicesid_list_ever", "@devicesid[0]", "id")
-				local deviceid_str = luci.sys.exec("uci get devicesid_list.@devicesid[0].id")
+				--local deviceid_str = luci.sys.exec("uci get devicesid_list.@devicesid[0].id")
 			    -- print(deviceid_str)	
 			    -- for key, value in pairs(deviceid_str) do 
-			    local match = string.match(deviceid_str,deviceId)
-	            if  match == nil then
-		            print( deviceId .. "not in devicelist" );
-		            return;
-		        end
+			    --local match = string.match(deviceid_str,deviceId)
+	            --if  match == nil then
+		      --      print( deviceId .. "not in devicelist" );
+		       --     return;
+		      --  end
                 
-				local queryParam = "gatewayid=" .. gatewayId .. "&deviceid=" .. deviceId;               
+				local queryParam = "{device_sn:\"".. deviceId .. "\"";               
 				
-				queryParam = queryParam .. "&devicetype=";
-				local deviceType = msg["devicetype"];
-				if deviceType ~= nil then
-					queryParam = queryParam .. deviceType;
-				end
-
-				queryParam = queryParam .. "&attr=";
+				queryParam = queryParam .. ",attr_code:";
 				local attr = msg["attr"];
 				if attr ~= nil then
-					queryParam = queryParam .. attr;
+					queryParam = queryParam .. "\"" ..attr .."\"";
 				end
 
-				queryParam = queryParam .. "&data=";
+				queryParam = queryParam .. ",attr_value:";
 				local data = msg["data"];
 				if data ~= nil then
-					queryParam = queryParam .. data;
+					queryParam = queryParam .. "\"" ..  data .. "\"";
 				end
-
+                                queryParam = queryParam .. ",source:\"user\""; 
+                                queryParam = queryParam .. ",gateway_sn:" .. "\"" .. gatewayId .. "\"}";
 				
-				local targetUrl = url .. queryParam;
 
-				print(targetUrl);
+				print(queryParam);
 
 				for i=1,3 do
-					resp, code = http.request(targetUrl);
+					--resp, code = http.request(targetUrl);
+
+
+
+                                        local response_body = {}  
+                                        --local post_data = '{"device_sn":"ttttt", "attr_code":666, "attr_value":567, "source":"user", "gateway_sn":"ffffff"}';  
+                                        res, code = http.request{  
+                                            url = url,  
+                                            method = "POST",  
+                                            headers =   
+                                            {  
+                                                ["Content-Type"] = "application/x-www-form-urlencoded",  
+                                                ["Content-Length"] = #queryParam,  
+                                            },  
+                                            source = ltn12.source.string(queryParam),  
+                                            sink = ltn12.sink.table(response_body)  
+                                        }  
+
+
 					if code == 200 then
 						conn:reply(req, {code="S00000", message="" .. resp});
 						return;
