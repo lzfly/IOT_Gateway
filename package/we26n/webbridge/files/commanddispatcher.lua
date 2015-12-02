69b83d83c2d943cb9b69096dd3e6199d5115b85d
luoie <luoiefly@163.com>
2015-12-02 13:01:44 +0800

    add setflag

--- package/we26n/webbridge/files/commanddispatcher.lua
+++ package/we26n/webbridge/files/commanddispatcher.lua
@@ -18,6 +18,7 @@ macAddr = string.gsub(macAddr,"%\n","")
 
 http = require "socket.http";
 url = "http://123.57.206.2:8081/v1/deviceattrinfos?query=gateway_sn:eq:" .. macAddr .. ",is_control:eq:1";
+url2 = "http://123.57.206.2:8081/v1/deviceattrinfos";
 
 conn = ubus.connect();
 if not conn then
@@ -32,7 +33,34 @@ function getCommand()
 
 	return nil;
 end
+function setFlag(attr_code)
+        queryParam = "{\"is_control\":0}";
 
+        local targetURL = url2 .. "/" .. attr_code
+
+        print(queryParam);
+        print(targetURL);
+
+        for i=1,3 do
+
+            local response_body = {}
+            res, code = http.request{
+                url = targetURL,
+                method = "PUT",
+                headers =
+                {
+                    ["Content-Type"] = "application/x-www-form-urlencoded",
+                    ["Content-Length"] = #queryParam,
+                },
+                source = ltn12.source.string(queryParam),
+                sink = ltn12.sink.table(response_body)
+            }
+            if code == 200 then
+                return;
+            end
+
+        end
+end
 function sendToZigbeeJianyou(gatewayid_value, deviceid_value, attr_value, data_value)
 --[[
 	local result = conn:call("we26n_zigbee_febee", "ctrlcmd", { gatewayid = gatewayid_value, deviceid = deviceid_value, devicetype = "0012", attr = attr_value, data = data_value });
@@ -87,14 +115,11 @@ function dispatchCommand(data)
         --print(dobj);
 	for idx, value in ipairs(dobj) do
 		if value ~= nil then
-                        print("luz");
 			local devId = value["device_sn"]
-                        print(devId)
 			if devId ~= nil then
 				local gatewayId = value["gateway_sn"];
 				local attr = value["attr_code"];
 				local data = value["attr_value_ctrl"];
-                                print(data);
 				local devIdLower = string.lower(devId);
 				if string.match(devIdLower, "zigbee_jianyou") == "zigbee_jianyou" then
 					sendToZigbeeJianyou(gatewayId, devId, attr, data);
@@ -103,6 +128,7 @@ function dispatchCommand(data)
 				elseif string.match(devIdLower, "433_jianyou") == "433_jianyou" then
 					sendTo433Jianyou(gatewayId, devId, attr, data);
 				end
+                                setFlag(attr);
 			end
 		end
 	end
@@ -123,6 +149,8 @@ function getWebServerURL()
     print(port)
     if iplen ~= 0 and portlen ~= 0 then
         url = "http://" .. ip .. ":" .. port .. "/v1/deviceattrinfos?query=gateway_sn:eq:" .. macAddr .. ",is_control:eq:1";
+        url2 = "http://" .. ip .. ":" .. port .. "/v1/deviceattrinfos";
+
     end
     print(url)
 
